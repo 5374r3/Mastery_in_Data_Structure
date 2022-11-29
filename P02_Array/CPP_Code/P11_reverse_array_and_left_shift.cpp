65e33e4c50e9641ad57344b30deee0684c0f6f35
saket61195 <saket61195@gmail.com>
2022-11-29 12:30:06 +0530

    reverse and left shift

---
+++ P02_Array/CPP_Code/P11_reverse_array_and_left_shift.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Array {
+    int A[20];
+    int size;
+    int length;
+};
+void Display(struct Array arr) {
+    for (int i = 0; i < arr.length; i++)
+        printf("%d ", arr.A[i]);
+    printf("\n");
+}
+void swap(int *x, int *y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int LinearSearch(struct Array arr, int key) {
+    for (int i = 0; i < arr.length; i++) {
+        if (key == arr.A[i])
+            return i;
+    }
+    return -1;
+}
+void Reverse(struct Array *arr) {
+    int *B;
+    B = (int *)malloc(arr->length * sizeof(int));
+    for (int i = arr->length - 1, j = 0; i >= 0; i--, j++)
+        B[j] = arr->A[i];
+    for (int i = 0; i < arr->length; i++)
+        arr->A[i] = B[i];
+    free(B);
+}
+void Reverse2(struct Array *arr) {
+    for (int i = 0, j = arr->length - 1; i < j; i++, j--)
+        swap(&arr->A[i], &arr->A[j]);
+}
+
+void LeftShift(struct Array *arr) {
+    int temp = arr->A[0];
+    for (int i = 0; i < arr->length; i++)
+        arr->A[i] = arr->A[i + 1];
+    arr->A[arr->length - 1] = temp;
+}
+
+int main() {
+    struct Array arr = {{2, 4, 8, 1, 5}, 20, 5};
+    Display(arr);
+    Reverse(&arr);
+    // Reverse2(&arr);
+    Display(arr);
+    LeftShift(&arr);
+    Display(arr);
+    LeftShift(&arr);
+    Display(arr);
+    return 0;
+}
+
+//  struct Array arr={{2, 4, 8, 1, 5},20,5};
+// {2, 4, 8, 1, 5} element of array
+// 20 size of array
+// 5 length of array
