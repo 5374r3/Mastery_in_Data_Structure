9c6befd36c0f9e25d93ec4edacce54d422110daf
saket61195 <saket61195@gmail.com>
2022-12-11 20:38:51 +0530

    duplicate string

--- P04_string/c_code/P10_find_duplicate_in_string.c
+++ P04_string/c_code/P10_find_duplicate_in_string.c
@@ -1,17 +1,88 @@
-#include<stdio.h>
-int main(){
-    char s[]="mohiinkknoo";
-    for (int i = 0; s[i] - 1; i++) {
+// #include <stdio.h>
+// #include<string.h>
+// int main() {
+//     char s[] = "mohiinkknoo";
+//     int count1 = 0;
+
+//     printf("%d\n", count1);
+//     for (int i = 0; i < count1 - 1; i++) {
+//         int count = 1;
+//         if (s[i] != -1)
+//             for (int j = i + 1; s[j] != '\0'; j++) {
+//                 if (s[i] == s[j]) {
+//                     count++;
+//                     s[j] = -1;
+//                 }
+//             }
+//         if (count > 1)
+//             printf("%c appaers %d times\n", s[i], count);
+//     }
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+//     char s[] = "mohiinkknoo";
+//     int x = strlen(s);
+//     printf("%d\n", x);
+//     for (size_t i = 0; i < strlen(s); i++) {
+//         int count = 1;
+//         if (s[i] != -1)
+//             for (int j = i + 1; s[j] != '\0'; j++) {
+//                 if (s[i] == s[j]) {
+//                     count++;
+//                     s[j] = -1;
+//                 }
+//             }
+//         if (count > 1)
+//             printf("%c appears %d times\n", s[i], count);
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+    char s[] = "mohiinkknoo";
+    int len;
+    for(len = 0; s[len]; len++);
+    // printf("%d\n",len);
+    for (int i = 0; i < len; i++) {
         int count = 1;
-        if (s[i] != -1)
-            for (int j = i + 1; s[j]!='\0'; j++) {
-                if (s[i] == s[j]) {
-                    count++;
-                    s[j] = -1;
-                }
+        if(s[i] != -1)
+        for (int j = i + 1; s[j] != '\0'; j++) {
+            if (s[i] == s[j]) {
+                count++;
+                s[j] = -1;
             }
+        }
         if (count > 1)
-            printf("%c appaers %d times\n", s[i], count);
+            printf("%c appears %d times\n", s[i], count);
     }
     return 0;
 }
+
+
+// #include <stdio.h>
+
+// int main() {
+//     char s[] = "mohiinkknoo";
+//     size_t len;
+//     for(len = 0; s[len]; len++);
+//     for (size_t i = 0; i < len; i++) {
+//         int count = 1;
+//         if(s[i] != -1)
+//         for (int j = i + 1; s[j] != '\0'; j++) {
+//             if (s[i] == s[j]) {
+//                 count++;
+//                 s[j] = -1;
+//             }
+//         }
+//         if (count > 1)
+//             printf("%c appears %d times\n", s[i], count);
+//     }
+//     return 0;
+// }
--- P04_string/cpp_code/P10_find_duplicate_in_string.cpp
+++ P04_string/cpp_code/P10_find_duplicate_in_string.cpp
@@ -1,20 +1,21 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main() {
-    char s[] = "nnikhoiiiiooooiln";
-    for (int i = 0; s[i] - 1; i++) {
+    char s[] = "mohiinkknoo";
+    int len;
+    for(len = 0; s[len]; len++);
+    // printf("%d\n",len);
+    for (int i = 0; i < len; i++) {
         int count = 1;
-        if (s[i] != -1) {
-            for (int j = i + 1; s[j] != '\0'; j++) {
-                if (s[j] == s[i]) {
-                    count++;
-                    s[j] = -1;
-                }
+        if(s[i] != -1)
+        for (int j = i + 1; s[j] != '\0'; j++) {
+            if (s[i] == s[j]) {
+                count++;
+                s[j] = -1;
             }
-            if (count > 1)
-                // printf("%c appaers %d times\n", s[i], count);
-                cout<< s[i]<<" appaers "<<count<<" times "<<endl;
         }
+        if (count > 1)
+            cout<<s[i]<<" appears "<<count<<" times "<<endl;;
     }
     return 0;
 }
